a3b20f8b92341a0b906bce7f2751d69356a203f6
Avner-Wang <847903302@qq.com>
2020-05-29 13:51:46 +0800

    添加学习清单功能

--- wyp-react/src/features_one.js
+++ wyp-react/src/features_one.js
@@ -3,20 +3,48 @@ import ReactDom from "react-dom";
 // import ReactScripts from "react-scripts";
 
 class Features_one extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "wypiscomeon",
+      list: ["英语", "历史", "数学", "政治"],
+    };
+  }
   render() {
     return (
       <Fragment>
         <div>
-          <input />
-          <button>添加学习清单列表</button>
+          <input
+            value={this.state.inputValue}
+            onChange={this.inputChange.bind(this)}
+          />
+          <button onClick={this.addList.bind(this)}>添加学习清单列表</button>
         </div>
         <ul>
-          <li>英语</li>
+          {this.state.list.map((item, index) => {
+            return <li key={index + item}>{item}</li>;
+          })}
+          {/* <li>英语</li>
           <li>历史</li>
-          <li>数学</li>
+          <li>数学</li> */}
         </ul>
       </Fragment>
     );
+  }
+
+  inputChange(e) {
+    console.log(e);
+    // this.state.inputValue = e.target.value;
+    this.setState({
+      inputValue: e.target.value,
+    });
+    console.log(this);
+  }
+  addList() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      // list: ['英语','历史','数学','政治',this.state.list, this.state.inputValue],
+    });
   }
 }
 
